ed900fa1fd362a43473621bee95c80059b5f86d9
Alexander Kharichev <myx@myx.co.nz>
2018-05-17 22:31:20 +0300

    * env or argument

--- sh-scripts/install-myx-repository-macosx.sh
+++ sh-scripts/install-myx-repository-macosx.sh
@@ -6,7 +6,7 @@
 ##       file-system) and on un-prepared OS. 
 ####
 
-TGT_APP_PATH="$TGT_APP_PATH"
+TGT_APP_PATH="$TGT_APP_PATH:-$1"
 [ -z "$TGT_APP_PATH" ] && echo "'TGT_APP_PATH' env must be set" >&2 && exit 1
 
 echo "Installing myx.distro repository into: $TGT_APP_PATH"
