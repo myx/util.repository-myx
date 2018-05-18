58abbc04d9547a22c7bb20c41c0c011d2b50e8ae
Alexander Kharichev <myx@myx.co.nz>
2018-05-18 12:01:13 +0300

    * myx.repo installer install myx.common

--- sh-scripts/install-myx-repository-freebsd.sh
+++ sh-scripts/install-myx-repository-freebsd.sh
@@ -6,4 +6,26 @@
 ##       file-system) and on un-prepared OS. 
 ####
 
-PATH="$1"
+test `id -u` = 0 || (echo "Must be root" >&2 && exit 1)
+
+TGT_APP_PATH="${TGT_APP_PATH:-$1}"
+test -z "$TGT_APP_PATH" && echo "'TGT_APP_PATH' env must be set" >&2 && exit 1
+
+##########################
+echo "Installing packages"
+
+
+test -z "`which myx.common`" && (fetch http://myx.ru/distro/farm-general/myx.common/install-bsd.sh -o - | sh -e)
+test -z "`which bash`" -o -z "`which git`" && pkg install -y bash git
+
+
+###########################################################
+echo "Installing myx.distro repository into: $TGT_APP_PATH"
+
+
+
+# download scripts and run them
+
+
+############
+echo "Done."
