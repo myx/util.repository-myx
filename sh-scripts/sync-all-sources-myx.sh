0c1fa93b6f86d6b21bbb2d5158935409af61f3b6
Alexander Kharichev <myx@myx.co.nz>
2018-05-20 00:03:33 +0300

    * myx.common moved to separate folder

--- sh-scripts/sync-all-sources-myx.sh
+++ sh-scripts/sync-all-sources-myx.sh
@@ -9,19 +9,20 @@ fi
 
 . "$APP/source/myx/util.repository-myx/sh-lib/syncing.sh.include"
 
-async gitSync "myx/util.repository-myx/"	"git@github.com:myx/util.repository-myx.git"
+async gitSync "myx/util.repository-myx/"	           "git@github.com:myx/util.repository-myx.git"
 
-async gitSync "myx/clean-boot/"             "git@github.com:myx/clean-boot.git"
-async gitSync "myx/clean-jdbc.util/"        "git@github.com:myx/clean-jdbc.util.git"
-async gitSync "myx/clean-yaumnrc/"          "git@github.com:myx/clean-yaumnrc.git"
+async gitSync "myx/clean-boot/"                        "git@github.com:myx/clean-boot.git"
+async gitSync "myx/clean-jdbc.util/"                   "git@github.com:myx/clean-jdbc.util.git"
+async gitSync "myx/clean-yaumnrc/"                     "git@github.com:myx/clean-yaumnrc.git"
 
 wait
 
-async gitSync "myx/os-myx.common/"          "git@github.com:myx/os-myx.common.git"
-async gitSync "myx/os-myx.common-freebsd/"  "git@github.com:myx/os-myx.common-freebsd.git"
-async gitSync "myx/os-myx.common-ubuntu/"   "git@github.com:myx/os-myx.common-ubuntu.git"
+async gitSync "myx/myx.common/os-myx.common/"          "git@github.com:myx/os-myx.common.git"
+async gitSync "myx/myx.common/os-myx.common-freebsd/"  "git@github.com:myx/os-myx.common-freebsd.git"
+async gitSync "myx/myx.common/os-myx.common-ubuntu/"   "git@github.com:myx/os-myx.common-ubuntu.git"
+async gitSync "myx/myx.common/os-myx.common-macosx/"   "git@github.com:myx/os-myx.common-macosx.git"
 
-async gitSync "myx/myx.os-java/"            "git@github.com:myx/myx.os-java.git"
+async gitSync "myx/myx.os-java/"                       "git@github.com:myx/myx.os-java.git"
 
 wait
 
