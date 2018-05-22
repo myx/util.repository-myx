377ec934c058eb9268bc9931cecbee54c9142ffb
Alexander Kharichev <myx@myx.co.nz>
2018-05-22 09:46:36 +0300

    * new async

--- sh-scripts/sync-all-sources-myx.sh
+++ sh-scripts/sync-all-sources-myx.sh
@@ -9,20 +9,20 @@ fi
 
 . "$APP/source/myx/util.repository-myx/sh-lib/syncing.sh.include"
 
-async gitSync "myx/util.repository-myx/"	           "git@github.com:myx/util.repository-myx.git"
+async -2 gitSync "myx/util.repository-myx/"					"git@github.com:myx/util.repository-myx.git"
 
-async gitSync "myx/clean-boot/"                        "git@github.com:myx/clean-boot.git"
-async gitSync "myx/clean-jdbc.util/"                   "git@github.com:myx/clean-jdbc.util.git"
-async gitSync "myx/clean-yaumnrc/"                     "git@github.com:myx/clean-yaumnrc.git"
+async -2 gitSync "myx/clean-boot/"                        	"git@github.com:myx/clean-boot.git"
+async -2 gitSync "myx/clean-jdbc.util/"                   	"git@github.com:myx/clean-jdbc.util.git"
+async -2 gitSync "myx/clean-yaumnrc/"                     	"git@github.com:myx/clean-yaumnrc.git"
 
 wait
 
-async gitSync "myx/myx.common/os-myx.common/"          "git@github.com:myx/os-myx.common.git"
-async gitSync "myx/myx.common/os-myx.common-freebsd/"  "git@github.com:myx/os-myx.common-freebsd.git"
-async gitSync "myx/myx.common/os-myx.common-ubuntu/"   "git@github.com:myx/os-myx.common-ubuntu.git"
-async gitSync "myx/myx.common/os-myx.common-macosx/"   "git@github.com:myx/os-myx.common-macosx.git"
+async -2 gitSync "myx/myx.common/os-myx.common/"          	"git@github.com:myx/os-myx.common.git"
+async -2 gitSync "myx/myx.common/os-myx.common-freebsd/"  	"git@github.com:myx/os-myx.common-freebsd.git"
+async -2 gitSync "myx/myx.common/os-myx.common-ubuntu/"   	"git@github.com:myx/os-myx.common-ubuntu.git"
+async -2 gitSync "myx/myx.common/os-myx.common-macosx/"   	"git@github.com:myx/os-myx.common-macosx.git"
 
-async gitSync "myx/myx.os-java/"                       "git@github.com:myx/myx.os-java.git"
+async -2 gitSync "myx/myx.os-java/"                       	"git@github.com:myx/myx.os-java.git"
 
 wait
 
